7bca6fc30c53b7df05ae7e917b479138f2c0da48
PonzRyu <ryu.ponz.dev.0112@gmail.com>
2026-08-25 13:18:42 +0900

    version-rename (#38)

--- src/parts/BottomBar.tsx
+++ src/parts/BottomBar.tsx
@@ -8,7 +8,7 @@ interface BottomBarProps {
 }
 
 export const BottomBar = ({
-  version = '1.1.0',
+  version = '1.1.1',
   author = 'https://github.com/PonzRyu',
 }: BottomBarProps) => {
   return (
